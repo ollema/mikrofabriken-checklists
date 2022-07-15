11389ca1d5c4fd97166e43d705f0c4dcde58a530
Olle Månsson <31876997+ollema@users.noreply.github.com>
2022-07-15 23:39:38 +0200

    start working on routing

---
+++ src/lib/machines/Index.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import { Route } from 'tinro';
+
+	import Setup from './setup/Index.svelte';
+
+	import Nav from './Nav.svelte';
+	import MachineButton from './MachineButton.svelte';
+
+	import type { MachineType } from '../../models/machine';
+
+	export let currentMachine: MachineType;
+</script>
+
+<Route path="/machines/:slug" let:meta>
+	<Setup {currentMachine} />
+</Route>
+
+<Nav {currentMachine} />
+
+<div class="default-width-padding mt-16">
+	<div class="flex justify-center">
+		<h1 class="font-medium text-xl">
+			checklists for the <strong>{currentMachine.title.toLowerCase()}</strong> machine
+		</h1>
+	</div>
+	<div class="flex justify-center mt-8 space-x-8">
+		<div>
+			<MachineButton {currentMachine} action="setup" />
+		</div>
+		<div>
+			<MachineButton {currentMachine} action="operate" />
+		</div>
+		<div>
+			<MachineButton {currentMachine} action="cleanup" />
+		</div>
+	</div>
+</div>
---
+++ src/lib/machines/setup/Index.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import Nav from './Nav.svelte';
+	import SetupStep from './SetupStepCard.svelte';
+
+	import type { MachineType } from '../../../models/machine';
+
+	export let currentMachine: MachineType;
+</script>
+
+<Nav {currentMachine} />
+
+<div class="default-width-padding mt-8">
+	<div class="flex justify-center">
+		<h1 class="font-medium text-xl">
+			the setup for the <strong>{currentMachine.title.toLowerCase()}</strong> machine consists of the following steps:
+		</h1>
+	</div>
+	<div class="flex flex-col items-center mt-8 space-y-2">
+		{#each currentMachine.setupSteps as setupStep, i}
+			<SetupStep {setupStep} {i} />
+		{/each}
+	</div>
+	<div class="flex flex-col items-center mt-6">
+		<a class="p-4 bg-green-600 text-white rounded-md shadow-md" href="/machines/{currentMachine.slug}/setup/step/0"
+			>let's get started!
+		</a>
+	</div>
+</div>
