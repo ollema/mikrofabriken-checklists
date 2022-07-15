83be3f64916c4f913ac1f71ddf8b929b3a927203
Olle Månsson <olle.mansson@zenseact.com>
2022-07-16 01:00:54 +0200

    a lot of progress!

--- src/lib/machines/setup/Index.svelte
+++ src/lib/machines/setup/Index.svelte
@@ -1,4 +1,8 @@
 <script lang="ts">
+	import { Route } from 'tinro';
+
+	import SetupStepPage from './step/Index.svelte';
+
 	import Nav from './Nav.svelte';
 	import SetupStep from './SetupStepCard.svelte';
 
@@ -7,22 +11,28 @@
 	export let currentMachine: MachineType;
 </script>
 
-<Nav {currentMachine} />
+<Route path="/">
+	<Nav {currentMachine} />
 
-<div class="default-width-padding mt-8">
-	<div class="flex justify-center">
-		<h1 class="font-medium text-xl">
-			the setup for the <strong>{currentMachine.title.toLowerCase()}</strong> machine consists of the following steps:
-		</h1>
+	<div class="default-width-padding mt-8">
+		<div class="flex justify-center">
+			<h1 class="font-medium text-xl">
+				the setup for the <strong>{currentMachine.title.toLowerCase()}</strong> machine consists of the following steps:
+			</h1>
+		</div>
+		<div class="flex flex-col items-center mt-8 space-y-2">
+			{#each currentMachine.setupSteps as setupStep, i}
+				<SetupStep {setupStep} {i} />
+			{/each}
+		</div>
+		<div class="flex flex-col items-center mt-6">
+			<a class="p-4 bg-green-600 text-white rounded-md shadow-md" href="/machines/{currentMachine.slug}/setup/step/0"
+				>let's get started!
+			</a>
+		</div>
 	</div>
-	<div class="flex flex-col items-center mt-8 space-y-2">
-		{#each currentMachine.setupSteps as setupStep, i}
-			<SetupStep {setupStep} {i} />
-		{/each}
-	</div>
-	<div class="flex flex-col items-center mt-6">
-		<a class="p-4 bg-green-600 text-white rounded-md shadow-md" href="/machines/{currentMachine.slug}/setup/step/0"
-			>let's get started!
-		</a>
-	</div>
-</div>
+</Route>
+
+<Route path="/step/:id/*" let:meta>
+	<SetupStepPage {currentMachine} currentStep={+meta.params.id} />
+</Route>
